98dffb733e7be115b44d033aedd24edb08a9006e
KarrySingh9018 <kerry.singh9018@gmail.com>
2020-11-30 18:02:53 -0500

    added users microservice to the repository

---
+++ PhotoAppApiUsers/src/main/java/com/learning/photoapp/api/users/data/UserEntity.java
@@ -0,0 +1,124 @@
+package com.learning.photoapp.api.users.data;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class UserEntity implements Serializable {
+
+ 
+	private static final long serialVersionUID = -2731425678149216053L;
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@Column(nullable=false, length=50)
+	private String firstName;
+	
+	@Column(nullable=false, length=50)
+	private String lastName;
+	
+	@Column(nullable=false, length=120, unique=true)
+	private String email;
+	
+	@Column(nullable=false, unique=true)
+	private String userId;
+	
+	@Column(nullable=false, unique=true)	
+	private String encryptedPassword;
+	
+	
+	/**
+	 * @return id
+	 */
+	public long getId() {
+		return id;
+	}
+	
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	/**
+	 * @return lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	/**
+	 * @return email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	/**
+	 * @return userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+	
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	/**
+	 * @return encryptedPassword
+	 */
+	public String getEncryptedPassword() {
+		return encryptedPassword;
+	}
+	
+	/**
+	 * @param encryptedPassword the encryptedPassword to set
+	 */
+	public void setEncryptedPassword(String encryptedPassword) {
+		this.encryptedPassword = encryptedPassword;
+	}
+	
+	
+
+}
