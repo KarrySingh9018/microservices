fb60f282c157772b1efaa1b3e68bd7eddbfe2ac1
Ramanpreet Singh <57689321+Ramanpreet9018@users.noreply.github.com>
2020-12-01 17:31:00 -0500

    Update PhotoAppApiConfigServerApplicationTests.java

--- PhotoAppAPIConfigServer/src/test/java/com/learning/photoapp/api/PhotoAppApiConfigServerApplicationTests.java
+++ PhotoAppAPIConfigServer/src/test/java/com/learning/photoapp/api/PhotoAppApiConfigServerApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest // To facilitate SpringBoot Application Testing
 public class PhotoAppApiConfigServerApplicationTests {
 
 	@Test
