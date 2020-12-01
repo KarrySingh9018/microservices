3c79fc108c4937c075889020a7bb0a628f4312c5
Ramanpreet Singh <57689321+Ramanpreet9018@users.noreply.github.com>
2020-12-01 17:29:20 -0500

    Update PhotoAppApiConfigServerApplication.java

--- PhotoAppAPIConfigServer/src/main/java/com/learning/photoapp/api/PhotoAppApiConfigServerApplication.java
+++ PhotoAppAPIConfigServer/src/main/java/com/learning/photoapp/api/PhotoAppApiConfigServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableConfigServer // HTTP Resource API for External Configurations
 public class PhotoAppApiConfigServerApplication {
 
 	public static void main(String[] args) {
