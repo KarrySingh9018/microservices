81eb4991d4d11c0af11f3a6d5b0b27400869eb42
KarrySingh9018 <kerry.singh9018@gmail.com>
2020-12-01 09:57:53 -0500

    added Albums microservice to the repository

---
+++ PhotoAppApiAlbums/src/main/java/com/learning/photoapp/api/albums/service/AlbumsService.java
@@ -0,0 +1,9 @@
+package com.learning.photoapp.api.albums.service;
+
+
+import com.learning.photoapp.api.albums.data.AlbumEntity;
+import java.util.List;
+
+public interface AlbumsService {
+    List<AlbumEntity> getAlbums(String userId);
+}
