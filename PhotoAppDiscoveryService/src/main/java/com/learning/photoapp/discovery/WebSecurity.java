fd8900d30e031093a976751929537c551dbe3d65
Ramanpreet Singh <57689321+KarrySingh9018@users.noreply.github.com>
2020-12-01 22:13:44 -0500

    updated websecurity.java

--- PhotoAppDiscoveryService/src/main/java/com/learning/photoapp/discovery/WebSecurity.java
+++ PhotoAppDiscoveryService/src/main/java/com/learning/photoapp/discovery/WebSecurity.java
@@ -4,9 +4,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@EnableWebSecurity //Required Annotation for enabling Spring Security
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
+    /* Method serves as an authorization point for incoming requests and provides 
+    support for cross-site resource sharing */	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
