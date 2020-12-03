bdce0a65f44724630d0517885a0b7de8dc9c95e0
Ramanpreet Singh <57689321+KarrySingh9018@users.noreply.github.com>
2020-12-03 13:33:10 -0500

    updated UserResponseModel.java

--- PhotoAppApiUsers/src/main/java/com/learning/photoapp/api/users/ui/model/UserResponseModel.java
+++ PhotoAppApiUsers/src/main/java/com/learning/photoapp/api/users/ui/model/UserResponseModel.java
@@ -2,6 +2,7 @@ package com.learning.photoapp.api.users.ui.model;
 
 import java.util.List;
 
+/* Class acts as blueprint for the User Response Model */
 public class UserResponseModel {
 
     private String userId;
@@ -9,31 +10,45 @@ public class UserResponseModel {
     private String lastName;
     private String email;
     private List<AlbumResponseModel> albums;
-    
+    	/** 
+	* @return userId
+	*/
 	public String getUserId() {
 		return userId;
 	}
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+	/** 
+	* @return firstName
+	*/
 	public String getFirstName() {
 		return firstName;
 	}
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	/** 
+	* @return lastName
+	*/
 	public String getLastName() {
 		return lastName;
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	/** 
+	* @return email
+	*/
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	/** 
+	* @return albums
+	*/
 	public List<AlbumResponseModel> getAlbums() {
 		return albums;
 	}
