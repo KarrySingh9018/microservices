21887ea3359b285093f56ffccf5f97a2830f768b
Ramanpreet Singh <57689321+KarrySingh9018@users.noreply.github.com>
2020-12-03 12:06:58 -0500

    Update UsersRepository.java

--- PhotoAppApiUsers/src/main/java/com/learning/photoapp/api/users/data/UsersRepository.java
+++ PhotoAppApiUsers/src/main/java/com/learning/photoapp/api/users/data/UsersRepository.java
@@ -2,6 +2,8 @@ package com.learning.photoapp.api.users.data;
 
 import org.springframework.data.repository.CrudRepository;
 
+/* This interface specifies methods for querying DB / Repository 
+to retrieve records using different fields */
 public interface UsersRepository extends CrudRepository<UserEntity, Long> {
 	UserEntity findByEmail(String email);
 	UserEntity findByUserId(String userId);
