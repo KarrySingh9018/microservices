48b913eb3c31a2c9f4bddafb1a8822ac02bb475b
Ramanpreet Singh <57689321+KarrySingh9018@users.noreply.github.com>
2020-12-03 13:23:44 -0500

    updated AlbumsResponseModel.java

--- PhotoAppApiUsers/src/main/java/com/learning/photoapp/api/users/ui/model/AlbumResponseModel.java
+++ PhotoAppApiUsers/src/main/java/com/learning/photoapp/api/users/ui/model/AlbumResponseModel.java
@@ -1,30 +1,43 @@
 package com.learning.photoapp.api.users.ui.model;
 
+/* Class acts as a response model of Albums Entity */
 public class AlbumResponseModel {
 
     private String albumId;
     private String userId; 
     private String name;
     private String description;
-    
+    	
+	/**
+	* @return albumId
+	*/
 	public String getAlbumId() {
 		return albumId;
 	}
 	public void setAlbumId(String albumId) {
 		this.albumId = albumId;
 	}
+	/**
+	* @return userId
+	*/
 	public String getUserId() {
 		return userId;
 	}
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+	/**
+	* @return name
+	*/
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+	* @return description
+	*/
 	public String getDescription() {
 		return description;
 	}
