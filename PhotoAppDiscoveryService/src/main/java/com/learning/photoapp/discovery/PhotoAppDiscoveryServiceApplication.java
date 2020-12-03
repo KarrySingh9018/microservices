9606380d1a1d8dbe78dab76d90382821b0f0a764
Ramanpreet Singh <57689321+KarrySingh9018@users.noreply.github.com>
2020-12-03 10:54:02 -0500

    Update PhotoAppDiscoveryServiceApplication.java

--- PhotoAppDiscoveryService/src/main/java/com/learning/photoapp/discovery/PhotoAppDiscoveryServiceApplication.java
+++ PhotoAppDiscoveryService/src/main/java/com/learning/photoapp/discovery/PhotoAppDiscoveryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //Registers the application as Eureka Server
 public class PhotoAppDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
