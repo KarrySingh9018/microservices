4b140e6e47bfcd9e27e36994ff5e28b01056c20e
Ramanpreet Singh <57689321+KarrySingh9018@users.noreply.github.com>
2020-12-03 12:19:03 -0500

    Update UsersService.java

--- PhotoAppApiUsers/src/main/java/com/learning/photoapp/api/users/service/UsersService.java
+++ PhotoAppApiUsers/src/main/java/com/learning/photoapp/api/users/service/UsersService.java
@@ -4,6 +4,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.learning.photoapp.api.users.shared.UserDto;
 
+/* Interface provides with methods used for querying repository
+using the methods provided in UserDetailsService interface. */
 public interface UsersService extends UserDetailsService{
 	UserDto createUser(UserDto userDetails);
 	UserDto getUserDetailsByEmail(String email);
